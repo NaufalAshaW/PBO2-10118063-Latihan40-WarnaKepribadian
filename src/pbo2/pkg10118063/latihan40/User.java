f86d7297d83b4afa1e022bc523128ebe915dfa70
Naufal Asha <55681676+NaufalAshaW@users.noreply.github.com>
2019-10-16 19:20:33 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118063/latihan40/User.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pkg10118063.latihan40;
+
+/**
+ *
+ * @author 
+ * NAMA   : Naufal Asha
+ * KELAS  : IF-2
+ * NIM    : 10118063
+ */
+public class User {
+    private String nama, warna;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+}
